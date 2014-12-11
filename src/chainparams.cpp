e080562a95ffe1b735469aae00732ba2da1eed64
Benjamin Chrobot <bchrobot@mit.edu>
2014-12-11 03:08:52 -0500

    correct params...

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -59,9 +59,9 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime    = 1300000000;
-        genesis.nBits    = 0x1e0fffff;
-        genesis.nNonce   = 0;
+        genesis.nTime    = 1418280815;
+        genesis.nBits    = 0x1e08ffff;
+        genesis.nNonce   = 166699;
         
         //// debug print
         hashGenesisBlock = genesis.GetHash();
